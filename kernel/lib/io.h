2bf0724fd781d796db228e2eed530b6606291f4f
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2023-07-06 01:43:28 +0800

    fix formatting

--- kernel/lib/io.h
+++ kernel/lib/io.h
@@ -4,13 +4,13 @@
 namespace io {
 
   extern inline uint8_t inb(uint16_t portnum) {
-      uint8_t data = 0;
-      asm volatile("inb %0, %1" : "=a" (data) : "Nd"(portnum));
-      return data;
+    uint8_t data = 0;
+    asm volatile("inb %0, %1" : "=a" (data) : "Nd"(portnum));
+    return data;
   }
 
   extern inline void outb(uint16_t portnum, uint8_t data) {
-      asm volatile("outb %1, %0" ::"a" (data), "Nd"(portnum));
+    asm volatile("outb %1, %0" ::"a" (data), "Nd"(portnum));
   }
 
 }
