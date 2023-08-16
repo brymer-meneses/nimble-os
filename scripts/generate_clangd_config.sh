e11554010e431bdfddc229ab7ac242bf83c0b069
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2023-08-17 06:37:48 +0800

    fixup! add appropriate flags to clangd

--- scripts/generate_clangd_config.sh
+++ scripts/generate_clangd_config.sh
@@ -6,18 +6,32 @@ output_file=".clangd"
 # Delete the existing output file if it exists
 rm -f "$output_file"
 
-cat <<EOT>> "$output_file"
-CompileFlags:
-  Add:
-    - "-I$project_dir/"
-    - "-I$project_dir/lib/thirdparty/libc++/include"
-    - "-I$project_dir/lib/thirdparty/"
-    - "-std=c++20"
-    - "-DENABLE_TESTS"
-    - "-Wpedantic"
-    - "-Wextra"
-    - "-Wall"
+# Initialize empty flag array
+compile_flags=(
+  "-I$project_dir/"
+  "-I$project_dir/lib/thirdparty/libc++/include"
+  "-I$project_dir/lib/thirdparty/"
+  "-std=c++20"
+  "-Wpedantic"
+  "-Wextra"
+  "-Wall"
+)
 
+# Check for command-line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -D*)
+      compile_flags+=("$1")
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+cat <<EOT>> "$output_file"
 Diagnostics:
   ClangTidy:
     Add: 
@@ -25,4 +39,11 @@ Diagnostics:
     Remove:
       - misc-unused-alias-decls
       - modernize-use-nodiscard
+
+CompileFlags:
+  Add:
 EOT
+
+for flag in "${compile_flags[@]}"; do
+  echo "      - \"$flag\"" >> "$output_file"
+done
