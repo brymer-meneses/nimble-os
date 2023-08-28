a22edd7bb443b4d99f537630f77ddbb20f5585ab
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2023-08-28 17:35:07 +0800

    fix format test

--- tests/syslib/test_format.cc
+++ tests/syslib/test_format.cc
@@ -1,45 +1,53 @@
 #include <lib/syslib/format.h>
 #include <tests/prelude.h>
 
-// currently broken :<
+struct TestWriter : sl::FormatWriter {
+  char buffer[1024];
+  int i = 0;
+  auto writeChar(const char character) -> void final {
+    buffer[i] = character;
+    buffer[i+1] = '\0';
+    i += 1;
+  };
 
-// TEST(Format, NumberFormat) {
-//
-//   char buffer[256];
-//
-//   sl::format(buffer, "The meaning of life is: {}!", 42);
-//
-//   assertEq(buffer, "The meaning of life is: 42!");
-// }
-//
-// TEST(Format, StringFormat) {
-//
-//   char buffer[256];
-//
-//   sl::format(buffer, "The quick brown {} jumped over the lazy {}.", "fox", "cat");
-//
-//   assertEq(buffer, "The quick brown fox jumped over the lazy cat.");
-//
-// }
-//
-// TEST(Format, Hex) {
-//   char buffer[256];
-//   sl::format(buffer, "{}-{hex}", 314159265, 314159265);
-//   assertEq(buffer, "314159265-0x12B9B0A1");
-//   sl::format(buffer, "{} TEST {hex}-{}", "HEX", 314159265, 314159265);
-//   assertEq(buffer, "HEX TEST 0x12B9B0A1-314159265");
-// }
-//
-// TEST(Format, Binary) {
-//   char buffer[256];
-//
-//   sl::format(buffer, "{}-{bin}", 2048, 2048);
-//   assertEq(buffer, "2048-0b100000000000");
-// }
-//
-// TEST(Format, Octal) {
-//   char buffer[256];
-//
-//   sl::format(buffer, "{}-{oct}", 2048, 2048);
-//   assertEq(buffer, "2048-0o4000");
-// }
+  auto reset() -> void {
+    i = 0;
+  }
+};
+
+TestWriter writer{};
+
+TEST(Format, NumberFormat) {
+  writer.reset();
+
+  sl::format(writer, "The meaning of life is: {}!", 42);
+  assertEq(writer.buffer, "The meaning of life is: 42!");
+}
+
+TEST(Format, StringFormat) {
+  writer.reset();
+
+  sl::format(writer, "The quick brown {} jumped over the lazy {}.", "fox", "cat");
+  assertEq(writer.buffer, "The quick brown fox jumped over the lazy cat.");
+}
+
+TEST(Format, Hex) {
+  writer.reset();
+
+  sl::format(writer, "{} TEST {#0x}-{}", "HEX", 314159265, 314159265);
+  assertEq(writer.buffer, "HEX TEST 0x12B9B0A1-314159265");
+}
+
+TEST(Format, Binary) {
+  writer.reset();
+
+  sl::format(writer, "{}-{#0b}", 2048, 2048);
+  assertEq(writer.buffer, "2048-0b100000000000");
+}
+
+TEST(Format, Octal) {
+  writer.reset();
+
+  sl::format(writer, "{}-{#0o}", 2048, 2048);
+  assertEq(writer.buffer, "2048-0o4000");
+}
