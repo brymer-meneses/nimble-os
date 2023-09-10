3cdc656a91f83a424606b91d5996b0c581986b7c
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2023-09-10 16:36:49 +0800

    improve clangd config generator

--- scripts/generate_clangd_config.py
+++ scripts/generate_clangd_config.py
@@ -30,22 +30,23 @@ def writeln(f, level, string):
     indents = '  ' * level
     f.write(f"{indents}{string}\n")
 
-with open(".clangd", "w") as f:
-    writeln(f, 0, "Diagnostics:")
-    writeln(f, 1, "ClangTidy:")
+if __name__ == "__main__":
+    with open(".clangd", "w") as f:
+        writeln(f, 0, "Diagnostics:")
+        writeln(f, 1, "ClangTidy:")
 
-    writeln(f, 2, "Add:")
-    for flag in clangtidy_flags["add"]:
-        writeln(f, 3, f"- {flag}")
+        writeln(f, 2, "Add:")
+        for flag in clangtidy_flags["add"]:
+            writeln(f, 3, f"- {flag}")
 
-    writeln(f, 2, "Remove:")
-    for flag in clangtidy_flags["remove"]:
-        writeln(f, 3, f"- {flag}")
+        writeln(f, 2, "Remove:")
+        for flag in clangtidy_flags["remove"]:
+            writeln(f, 3, f"- {flag}")
 
-    writeln(f, 0, "CompileFlags:")
-    writeln(f, 1, "Add:")
+        writeln(f, 0, "CompileFlags:")
+        writeln(f, 1, "Add:")
 
-    for flag in compile_flags:
-        writeln(f, 2, f"- \"-{flag}\"")
-    for dir in include_dirs:
-        writeln(f, 2, f"- \"-I{os.path.join(os.getcwd(),dir)}\"")
+        for flag in compile_flags:
+            writeln(f, 2, f'- "-{flag}"')
+        for dir in include_dirs:
+            writeln(f, 2, f'- "-I{os.path.join(os.getcwd(),dir)}"')
